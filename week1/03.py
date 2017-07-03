29e6b2018efc1effe389e11bb040cf2cb1ba0f2b
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-07-03 12:42:26 +0300

    first

--- week1/03.py
+++ week1/03.py
@@ -1,15 +1,21 @@
 import random
 
 secret = random.randint(1, 3)
+tries = 0
 
-while True:
+
+while tries<5:
+# while True:
 	try:
 		your = int(input('your?:'))
 	except ValueError:
 		continue
 		
+	tries +=1
 	if your == secret:
 		print('ok')
+		print(tries, 'tries')
 		break
-
+else:
+	print("???")
 

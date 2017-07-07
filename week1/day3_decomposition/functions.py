11d58f7c9c804038910d3334801156974693ba8b
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-07-07 11:26:58 +0300

    third

--- week1/day3_decomposition/functions.py
+++ week1/day3_decomposition/functions.py
@@ -1,6 +1,6 @@
+'''
 def f1(x, y):
     return x < y
-
 
 print(f1(ord("q"), 5))
 print(min(2, 3, 5, 6))
@@ -8,7 +8,6 @@ print(min(2, 3, 5, 6))
 
 def f2(x, y=2):
     return x * y
-
 
 print(f2(2, 3))
 print(f2(2))
@@ -39,3 +38,36 @@ def sum_(*args):
         s += arg
     return s
 print(sum_(3,2,4))
+'''
+
+def f5(x):
+    return 2*x
+
+double = f5
+print(double(2))
+
+
+def execute(f,x):
+    return f(x)
+print(execute(double,3))
+
+print(list(map(double, [1,2,3,4])))
+
+
+
+def odd(x):
+    return x % 2
+
+print(list(filter(odd, [1,2,3,4])))
+
+
+
+print(list(map(lambda x: 3*x, [1,2,3,4,5])))
+print(list(map(lambda x: 2*x, range(10))))
+print([3*x for x in range(10) if x%2])
+
+l = [(3,7),(2,8),(4,2)]
+l.sort()
+print(l)
+l.sort(key=lambda x: x[1])
+print(l)

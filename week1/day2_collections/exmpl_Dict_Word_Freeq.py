c0cc6fd6a20008eb2b5935f139ea8f546a068018
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-07-07 10:20:15 +0300

    second

--- week1/day2_collections/exmpl_Dict_Word_Freeq.py
+++ week1/day2_collections/exmpl_Dict_Word_Freeq.py
@@ -1,19 +1,19 @@
 import string
 
-print(dir(string))
+# print(dir(string))
 
 with open('pg1661.txt', 'rt') as f:
     # for line in f:
     #    print(line)
 
     text = f.read()
-    print(text)
+    # print(text)
 
     p = {ord(char): '' for char in string.punctuation}
-    print(p)
+    # print(p)
 
     words = text.lower().translate(p).split()
-    print(words)
+    # print(words)
 
     word_counter = {}
     for word in words:
@@ -22,7 +22,16 @@ with open('pg1661.txt', 'rt') as f:
         else:
             word_counter[word] = 1
 
-    print(word_counter)
-    print(len(word_counter))
+    # print(word_counter)
+    # print(len(word_counter))
 
-    # отсортировать слова по частоте и записать в файл
+    # sort by freequency and write to file
+    # sorted(word_counter)
+    # with open('w_pg1661.txt', 'wt') as f2:
+    #    for w in word_counter:
+    #        f2.write(w)
+    #        f2.write('\n')
+
+    counter_word = [(counter, word) for word, counter in word_counter.items()]
+    print(counter_word)
+

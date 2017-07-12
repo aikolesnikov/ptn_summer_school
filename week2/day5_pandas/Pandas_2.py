184cccad880b75481c44383b9e0fd02998b1b434
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2017-07-12 13:11:36 +0300

    fifth

--- week2/day5_pandas/Pandas_2.py
+++ week2/day5_pandas/Pandas_2.py
@@ -7,7 +7,8 @@ ts0 = pd.read_csv('~/prog/pyt/data/GlobalLandTemperaturesByCountry.csv', parse_d
 
 tsk3 = ts0.dropna().groupby('Country')['AverageTemperature'].aggregate(np.median).sort_values()
 print(tsk3.index)
-print(tsk3)
-tsk3.plot()
-plt.show()
+ds2 = ts0[ts0['AverageTemperature'].values >= 22].dropna()
+print(ds2)
+#tsk3.plot()
+#plt.show()
 
